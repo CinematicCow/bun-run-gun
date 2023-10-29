097f21f251aedd74b5688e3cfe65de1933427206
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2023-10-29 09:08:38 +0530

    fix: cli option for server port

--- index.ts
+++ index.ts
@@ -1,22 +1,35 @@
-import Gun from "gun";
-const program = require('commander');
+// use import in ts. As fas as I can tell, require does not import the types correctly.
+import { program } from 'commander';
 
-program
-    .option('-p, --port <number>', 'port number', 3000)
-    .parse(process.argv);
+/**
+ * Throw an error if port is not provided.
+ * parse() without passing in "process.argv" is impicit and auto-detects falgs.
+ * to add a default value to port, pass an additional argument as string with the
+ * value at the end.
+ * eg: program.requiredOption('-p, --port <number>', 'port number', '3000).parse();
+ */
+program.requiredOption('-p, --port <number>', 'port number').parse();
 
-const port = program.port;
+/**
+ * parse the provided port as a number
+ */
+const port: number = parseInt(program.opts().port, 10);
+
+/**
+ * If provided port is not a number, exit with error
+ */
+if (isNaN(port)) {
+  console.error('Invalid port provided');
+  process.exit(1);
+}
 
 console.log(`Port number: ${port}`);
 // Local imports
-import { app } from "./app"
-
-
+import { app } from './app';
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 // Start Gunconst { program } = require('commander');
-
